449143022b43c66725ce8dd46cf965c8025723c4
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-13 18:57:21 +0000

    metamod environment

--- experiments/local_run.sh
+++ experiments/local_run.sh
@@ -1,4 +1,4 @@
-python slow_task_switch.py --run-name slow_switch_ --datasets AffineCorrelatedGaussian --save-path "../results/task_switch_less_iters_local/" --change-task-every 1500
-python slow_task_switch.py --run-name slow_switch_ --datasets MultiDimGaussian --save-path "../results/task_switch_less_iters_local/" --change-task-every 1500
-python slow_task_switch.py --run-name mid_switch_ --datasets AffineCorrelatedGaussian --save-path "../results/task_switch_less_iters_local/" --change-task-every 600
-python slow_task_switch.py --run-name mid_switch_ --datasets MultiDimGaussian --save-path "../results/task_switch_less_iters_local/" --change-task-every 600
+python slow_task_switch.py --run-name slow_switch_ --datasets AffineCorrelatedGaussian --save-path "../results/task_switch_new_env/" --change-task-every 1500
+# python slow_task_switch.py --run-name slow_switch_ --datasets MultiDimGaussian --save-path "../results/task_switch_less_iters_local/" --change-task-every 1500
+# python slow_task_switch.py --run-name mid_switch_ --datasets AffineCorrelatedGaussian --save-path "../results/task_switch_less_iters_local/" --change-task-every 600
+# python slow_task_switch.py --run-name mid_switch_ --datasets MultiDimGaussian --save-path "../results/task_switch_less_iters_local/" --change-task-every 600
