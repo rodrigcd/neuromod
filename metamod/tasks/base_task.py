e9a453bf4d595838a946fb369a752c41467c7331
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-16 18:50:43 +0000

    some weird Q thingy

--- metamod/tasks/base_task.py
+++ metamod/tasks/base_task.py
@@ -1,7 +1,17 @@
+import numpy as np
+
+
 class BaseTask(object):
 
     def sample_batch(self):
         pass
 
     def get_correlation_matrix(self):
-        pass
+        return [[] for i in range(5)]
+
+    def get_linear_regression_solution(self, reg_coef=0.0):
+        correlation_matrices = self.get_correlation_matrix()
+        input_corr = correlation_matrices[0]
+        input_output_corr = correlation_matrices[2]
+        linear_mapping = input_output_corr.T @ np.linalg.inv(input_corr)
+        return linear_mapping
