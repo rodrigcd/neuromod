028d99c5e8a18d1a48a1889614beb06db7765d06
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-12-27 17:07:29 +0000

    fixing logs and running multiple experiments on cluster

--- experiments/slurm_scripts/single_task.sh
+++ experiments/slurm_scripts/single_task.sh
@@ -2,10 +2,13 @@ nvidia-smi
 python -c "import torch;print(torch.cuda.is_available())"
 
 cd ..
-experiment_path="long_run_non_linear"
+experiment_path="two_layer_linear"
 save_path="../results/$experiment_path/"
 echo $save_path
 
-python linear_two_layer.py --dataset "AffineCorrelatedGaussian" --n-steps 200 --iter-control 10
-python linear_two_layer.py --dataset "Semantic" --n-steps 200 --iter-control 10
-python linear_two_layer.py --dataset "MNIST" --n-steps 200 --iter-control 10
+python linear_two_layer.py --dataset "AffineCorrelatedGaussian" --run-name "run_id_$1" --save-path $save_path\
+ --n-steps 10000 --iter-control 500
+python linear_two_layer.py --dataset "Semantic" --run-name "run_id_$1" --save-path $save_path\
+ --n-steps 10000 --iter-control 500
+python linear_two_layer.py --dataset "MNIST" --run-name "run_id_$1" --save-path $save_path\
+ --n-steps 10000 --iter-control 500
