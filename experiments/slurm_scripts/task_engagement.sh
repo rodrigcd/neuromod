dbb0f5db5f6142271aa4aae839f447aaf41e3c48
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2023-01-19 22:50:58 +0000

    run task engagement and category assimilation with vector attention and steps in the semantic task

--- experiments/slurm_scripts/task_engagement.sh
+++ experiments/slurm_scripts/task_engagement.sh
@@ -6,5 +6,6 @@ experiment_path="task_engagement"
 save_path="../results/$experiment_path/"
 echo $save_path
 
-python task_engagement.py --run-name "run_id_$1" --save-path $save_path --run-id $1 --engage-type "active"
+#python task_engagement.py --run-name "run_id_$1" --save-path $save_path --run-id $1 --engage-type "active"
 #python task_engagement.py --run-name "run_id_$1" --save-path $save_path --run-id $1 --engage-type "attention"
+python task_engagement.py --run-name "run_id_$1" --save-path $save_path --run-id $1 --engage-type "vector"
