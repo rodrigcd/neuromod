0b1655e9c53290257343dba1218fb35554b92b0d
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2023-11-14 21:32:42 +0000

    rebuttal

--- experiments/slurm_scripts/maml_sweep.sh
+++ experiments/slurm_scripts/maml_sweep.sh
@@ -2,9 +2,9 @@ nvidia-smi
 python -c "import torch;print(torch.cuda.is_available())"
 
 cd ..
-experiment_path="hr_maml_last_step_masking"
+experiment_path="hr_maml_masking"
 save_path="../results/$experiment_path/"
 echo $save_path
 
-# python maml.py --save-path $save_path --run-id $1
-python maml.py --save-path $save_path --run-id $1 --last-step True
+python maml.py --save-path $save_path --run-id $1
+# python maml.py --save-path $save_path --run-id $1 --last-step True
